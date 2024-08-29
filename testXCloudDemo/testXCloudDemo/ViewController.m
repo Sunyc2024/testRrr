fa64893472a00ff0503c5ee08f5829f9d87ed9db
Sunyc <987598765@qq.com>
2024-08-29 15:47:16 +0800

    增加了一个label

--- testXCloudDemo/testXCloudDemo/ViewController.m
+++ testXCloudDemo/testXCloudDemo/ViewController.m
@@ -8,7 +8,7 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+@property(nonatomic, strong)UILabel *testLab;
 @end
 
 @implementation ViewController
@@ -17,8 +17,18 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     self.view.backgroundColor = UIColor.orangeColor;
-    self.title = @"这是一个测试包";
+    [self.view addSubview:self.testLab];
 }
 
+-(UILabel *)testLab{
+    if (!_testLab) {
+        _testLab = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 200, 30)];
+        _testLab.center = self.view.center;
+        _testLab.text = @"这是一个测试包";
+        _testLab.textColor = UIColor.blackColor;
+        _testLab.textAlignment = NSTextAlignmentCenter;
+    }
+    return _testLab;
+}
 
 @end
